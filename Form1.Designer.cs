893398eef800269e85a13ba8b1384678d2e57192
Enrique <enriquemilham@gmail.com>
2020-08-07 14:10:41 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace EnriqueFractionalNumberGame
+{
+	partial class FormFractionalNumberGame
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.labelNum1 = new System.Windows.Forms.Label();
+			this.labelStrip1 = new System.Windows.Forms.Label();
+			this.labelDenom1 = new System.Windows.Forms.Label();
+			this.labelNum2 = new System.Windows.Forms.Label();
+			this.labelStrip2 = new System.Windows.Forms.Label();
+			this.labelDenom2 = new System.Windows.Forms.Label();
+			this.labelOperator = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBoxAnswerNum = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBoxAnswerDenom = new System.Windows.Forms.TextBox();
+			this.buttonCek = new System.Windows.Forms.Button();
+			this.buttonRestart = new System.Windows.Forms.Button();
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.label3 = new System.Windows.Forms.Label();
+			this.labelScore = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// labelNum1
+			// 
+			this.labelNum1.AutoSize = true;
+			this.labelNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelNum1.Location = new System.Drawing.Point(25, 53);
+			this.labelNum1.Name = "labelNum1";
+			this.labelNum1.Size = new System.Drawing.Size(61, 24);
+			this.labelNum1.TabIndex = 0;
+			this.labelNum1.Text = "Num1";
+			// 
+			// labelStrip1
+			// 
+			this.labelStrip1.AutoSize = true;
+			this.labelStrip1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelStrip1.Location = new System.Drawing.Point(25, 88);
+			this.labelStrip1.Name = "labelStrip1";
+			this.labelStrip1.Size = new System.Drawing.Size(60, 24);
+			this.labelStrip1.TabIndex = 1;
+			this.labelStrip1.Text = "_____";
+			// 
+			// labelDenom1
+			// 
+			this.labelDenom1.AutoSize = true;
+			this.labelDenom1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelDenom1.Location = new System.Drawing.Point(25, 133);
+			this.labelDenom1.Name = "labelDenom1";
+			this.labelDenom1.Size = new System.Drawing.Size(82, 24);
+			this.labelDenom1.TabIndex = 2;
+			this.labelDenom1.Text = "Denom1";
+			// 
+			// labelNum2
+			// 
+			this.labelNum2.AutoSize = true;
+			this.labelNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelNum2.Location = new System.Drawing.Point(195, 53);
+			this.labelNum2.Name = "labelNum2";
+			this.labelNum2.Size = new System.Drawing.Size(61, 24);
+			this.labelNum2.TabIndex = 3;
+			this.labelNum2.Text = "Num2";
+			// 
+			// labelStrip2
+			// 
+			this.labelStrip2.AutoSize = true;
+			this.labelStrip2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelStrip2.Location = new System.Drawing.Point(195, 88);
+			this.labelStrip2.Name = "labelStrip2";
+			this.labelStrip2.Size = new System.Drawing.Size(60, 24);
+			this.labelStrip2.TabIndex = 4;
+			this.labelStrip2.Text = "_____";
+			// 
+			// labelDenom2
+			// 
+			this.labelDenom2.AutoSize = true;
+			this.labelDenom2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelDenom2.Location = new System.Drawing.Point(195, 133);
+			this.labelDenom2.Name = "labelDenom2";
+			this.labelDenom2.Size = new System.Drawing.Size(82, 24);
+			this.labelDenom2.TabIndex = 5;
+			this.labelDenom2.Text = "Denom2";
+			// 
+			// labelOperator
+			// 
+			this.labelOperator.AutoSize = true;
+			this.labelOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelOperator.Location = new System.Drawing.Point(112, 82);
+			this.labelOperator.Name = "labelOperator";
+			this.labelOperator.Size = new System.Drawing.Size(85, 39);
+			this.labelOperator.TabIndex = 6;
+			this.labelOperator.Text = "Oprt";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(115, 9);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(222, 24);
+			this.label8.TabIndex = 7;
+			this.label8.Text = "Fractional Number Game";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(292, 88);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(21, 24);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "=";
+			// 
+			// textBoxAnswerNum
+			// 
+			this.textBoxAnswerNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxAnswerNum.Location = new System.Drawing.Point(331, 56);
+			this.textBoxAnswerNum.Name = "textBoxAnswerNum";
+			this.textBoxAnswerNum.Size = new System.Drawing.Size(61, 29);
+			this.textBoxAnswerNum.TabIndex = 9;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(327, 88);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(70, 24);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "______";
+			// 
+			// textBoxAnswerDenom
+			// 
+			this.textBoxAnswerDenom.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBoxAnswerDenom.Location = new System.Drawing.Point(331, 130);
+			this.textBoxAnswerDenom.Name = "textBoxAnswerDenom";
+			this.textBoxAnswerDenom.Size = new System.Drawing.Size(61, 29);
+			this.textBoxAnswerDenom.TabIndex = 11;
+			// 
+			// buttonCek
+			// 
+			this.buttonCek.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonCek.Location = new System.Drawing.Point(417, 88);
+			this.buttonCek.Name = "buttonCek";
+			this.buttonCek.Size = new System.Drawing.Size(87, 33);
+			this.buttonCek.TabIndex = 12;
+			this.buttonCek.Text = "Cek";
+			this.buttonCek.UseVisualStyleBackColor = true;
+			this.buttonCek.Click += new System.EventHandler(this.buttonCek_Click);
+			// 
+			// buttonRestart
+			// 
+			this.buttonRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonRestart.Location = new System.Drawing.Point(12, 198);
+			this.buttonRestart.Name = "buttonRestart";
+			this.buttonRestart.Size = new System.Drawing.Size(87, 32);
+			this.buttonRestart.TabIndex = 13;
+			this.buttonRestart.Text = "Restart";
+			this.buttonRestart.UseVisualStyleBackColor = true;
+			this.buttonRestart.Click += new System.EventHandler(this.buttonRestart_Click);
+			// 
+			// buttonExit
+			// 
+			this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.buttonExit.Location = new System.Drawing.Point(417, 198);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(87, 32);
+			this.buttonExit.TabIndex = 14;
+			this.buttonExit.Text = "Exit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(204, 198);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(70, 24);
+			this.label3.TabIndex = 15;
+			this.label3.Text = "Score :";
+			// 
+			// labelScore
+			// 
+			this.labelScore.AutoSize = true;
+			this.labelScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.labelScore.Location = new System.Drawing.Point(280, 198);
+			this.labelScore.Name = "labelScore";
+			this.labelScore.Size = new System.Drawing.Size(20, 24);
+			this.labelScore.TabIndex = 16;
+			this.labelScore.Text = "0";
+			// 
+			// FormFractionalNumberGame
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(516, 245);
+			this.Controls.Add(this.labelScore);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.buttonRestart);
+			this.Controls.Add(this.buttonCek);
+			this.Controls.Add(this.textBoxAnswerDenom);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBoxAnswerNum);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.labelOperator);
+			this.Controls.Add(this.labelDenom2);
+			this.Controls.Add(this.labelStrip2);
+			this.Controls.Add(this.labelNum2);
+			this.Controls.Add(this.labelDenom1);
+			this.Controls.Add(this.labelStrip1);
+			this.Controls.Add(this.labelNum1);
+			this.Name = "FormFractionalNumberGame";
+			this.Text = "Form Fractional Number Game";
+			this.Load += new System.EventHandler(this.FormFractionalNumberGame_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelNum1;
+		private System.Windows.Forms.Label labelStrip1;
+		private System.Windows.Forms.Label labelDenom1;
+		private System.Windows.Forms.Label labelNum2;
+		private System.Windows.Forms.Label labelStrip2;
+		private System.Windows.Forms.Label labelDenom2;
+		private System.Windows.Forms.Label labelOperator;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBoxAnswerNum;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBoxAnswerDenom;
+		private System.Windows.Forms.Button buttonCek;
+		private System.Windows.Forms.Button buttonRestart;
+		private System.Windows.Forms.Button buttonExit;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label labelScore;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using EnriqueMath;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnriqueFractionalNumberGame
+{
+	public partial class FormFractionalNumberGame : Form
+	{
+		public FormFractionalNumberGame()
+		{
+			InitializeComponent();
+		}
+
+		Random rnd = new Random();
+		int num1, num2, denom1, denom2,operators,score;
+		string operationString;
+		EnriqueFractionalNumber koreksi;
+
+		private void GenerateRandomEquation()
+		{
+			num1 = rnd.Next(1, 101);
+			num2 = rnd.Next(1, 101);
+			denom1 = rnd.Next(1, 101);
+			denom2 = rnd.Next(1, 101);
+			operators = rnd.Next(1, 5);
+
+			EnriqueFractionalNumber fract1 = new EnriqueFractionalNumber(num1, denom1);
+			EnriqueFractionalNumber fract2 = new EnriqueFractionalNumber(num2, denom2);
+
+			switch (operators)
+			{
+				case 1:
+					operationString = "+";
+					koreksi = fract1 + fract2;
+					break;
+
+				case 2:
+					operationString = "-";
+					koreksi = fract1 - fract2;
+					break;
+
+				case 3:
+					operationString = "/";
+					koreksi = fract1 / fract2;
+					break;
+
+				case 4:
+					operationString = "*";
+					koreksi = fract1 * fract2;
+					break;
+			}
+
+			labelNum1.Text = fract1.Numerator.ToString();
+			labelDenom1.Text = fract1.Denominator.ToString();
+			labelNum2.Text = fract2.Numerator.ToString();
+			labelDenom2.Text = fract2.Denominator.ToString();
+			labelOperator.Text = operationString;
+		}
+
+		private void FormFractionalNumberGame_Load(object sender, EventArgs e)
+		{
+			GenerateRandomEquation();
+		}
+
+		private void buttonCek_Click(object sender, EventArgs e)
+		{
+			EnriqueFractionalNumber fractResult = new EnriqueFractionalNumber(int.Parse(textBoxAnswerNum.Text), int.Parse(textBoxAnswerDenom.Text));
+			bool result = koreksi.IsEqual(fractResult);
+
+			if(result)
+			{
+				MessageBox.Show("Jawaban benar");
+				score += 10;
+			}
+			else
+			{
+				MessageBox.Show("Jawaban salah");
+			}
+
+			labelScore.Text = score.ToString();
+			GenerateRandomEquation();
+			textBoxAnswerNum.Text = textBoxAnswerDenom.Text = "";
+		}
+
+		private void buttonRestart_Click(object sender, EventArgs e)
+		{
+			DialogResult dr = MessageBox.Show("Are you sure to restart?", "Confirmation", MessageBoxButtons.YesNo);
+
+			if (dr == DialogResult.Yes)
+			{
+				score = 0;
+				labelScore.ToString();
+				textBoxAnswerNum.Text = textBoxAnswerDenom.Text = "";
+				GenerateRandomEquation();
+			}
+
+		}
+
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
